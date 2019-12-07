c8d09559c5bcfbae02112817f88cee3071ab645b
Sofi Xeno <46944287+SofiXeno@users.noreply.github.com>
2019-12-07 15:32:02 +0200

    app.js adding

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+
+//set the template engine ejs
+app.set('view engine', 'ejs')
+
+//middlewares
+app.use(express.static('public'))
+
+
+//routes
+app.get('/', (req, res) => {
+    //res.render('index')
+    res.send("hello world")
+})
+
+//Listen on port 3000
+server = app.listen(3000)
