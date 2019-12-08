db283a2f27eb463a1cc37233644ced2430283e58
Sofi Xeno <46944287+SofiXeno@users.noreply.github.com>
2019-12-08 21:04:57 +0200

    some changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const hash = require('password-hash')
+let online = 0;
 
 //set the template engine ejs
 app.set('view engine', 'ejs')
@@ -22,27 +23,131 @@ server = app.listen(3000)
 //socket.io instantiation
 const io = require("socket.io")(server)
 
+const mysql =require("mysql")
+
+const mysql_connection = mysql.createConnection({
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'b5fa048bf259db',
+    password: '39864794',
+    database: 'heroku_cb66a8c29e3eeb5',
+
+});
+mysql_connection.connect(function (err) {
+    if (err) {
+        console.log("SQL not connected")
+    } else {
+        console.log("SQL connected!")
+    }
+});
+setInterval(() => {
+    mysql_connection.query('SELECT 1', (error) => {
+        if (error) throw error
+    })
+}, 50000)
+
 
 //listen on every connection
 io.on('connection', (socket) => {
+    online++;
+
+    io.sockets.emit('online_changed', {online : online});
+
     console.log('New user connected')
 
     //default username
     socket.username = "Anonymous"
 
-    //listen on change_username
-    socket.on('change_username', (data) => {
-        socket.username = data.username
+    //listen on change_username.
+    socket.on('change_user', (data) => {
+        if(data.password === undefined || data.username === undefined || data.password.length ===0 || data.username.length === 0 )
+            return;
+        verifyUser(data, socket)
     })
 
     //listen on new_message
     socket.on('new_message', (data) => {
         //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+        data.username = socket.username
+        addMessageToDB(data, socket)
     })
 
     //listen on typing
     socket.on('typing', (data) => {
         socket.broadcast.emit('typing', {username : socket.username})
     })
-})
+
+    socket.on('disconnect', (socket) => {
+        online--
+        console.log('USER DISCONNECTED')
+        io.sockets.emit('online_changed', {online : online})
+
+    })
+
+    showMessages(socket)
+
+
+    });
+function showMessages(socket) {
+    mysql_connection.query(
+        'SELECT * FROM ChatDB',
+        [],
+        (error, row) => {
+            if(row!==undefined) {
+                for (let i = 0; i < row.length; ++i)
+                    socket.emit('new_message', {message: row[i].message, time: row[i].time, username: row[i].username})
+            }
+        }
+    );
+}
+const a = 0;
+
+
+
+    function registerNewUser(data, socket) {
+        mysql_connection.query(
+            "INSERT INTO `UserDB` VALUES (?, ?, ?)",
+            [data.username, hash.generate(data.password), 0],
+            () => {
+                console.log(data.username +  " is now registered!")
+                socket.username = data.username
+            }
+        )
+    }
+
+    function verifyUser(data, socket) {
+            mysql_connection.query(
+                'SELECT * FROM `UserDB` u WHERE u.username = ?',
+                [data.username],
+                (error, row) => {
+                    console.log(row);
+                    if (typeof row !== "undefined"&&row.length!=0) {
+                       let res = row[0]
+                        if (!hash.verify(data.password, res.password)) {
+                            console.log("incorrect password");
+                        } else {
+                            console.log("user is connected");
+                            socket.username = data.username;
+                             socket.role = res.role;
+                        }
+                    } else {
+                        console.log("creating new user");
+                        registerNewUser(data, socket);
+                    }
+                }
+            );
+    }
+
+function addMessageToDB(data) {
+    mysql_connection.query("INSERT INTO ChatDB (username, time, message) VALUES (?, ?, ?)", [data.username, data.time, data.message],
+        (err, res) => {
+        console.log(res)
+            if(typeof res !=="undefined") {
+                data.id = res.insertId;
+                io.sockets.emit("new_message", data);
+            }
+        });
+
+}
+
+
+
--- public/chat.js
+++ public/chat.js
@@ -5,26 +5,39 @@ $(function(){
     //buttons and inputs
     var message = $("#message")
     var username = $("#username")
+    var password = $("#password")
     var send_message = $("#send_message")
-    var send_username = $("#send_username")
+    var send_password = $("#send_password")
     var chatroom = $("#chat_body")
     var feedback = $("#feedback")
 
+    function time() {
+        let time = new Date()
+        let hrs = time.getHours()
+        let min = time.getMinutes()
+        let sec = time.getSeconds()
+        return (hrs < 10 ? "0" + hrs.toString() : hrs) + ":" + (min < 10 ? "0" + min.toString() : min) + ":" + (sec < 10 ? "0" + sec.toString() : sec)
+    }
+
     //Emit message
     send_message.click(function(){
-        socket.emit('new_message', {message : message.val()})
+        socket.emit('new_message', {message : message.val(), time: time()} )
     })
 
     //Listen on new_message
     socket.on("new_message", (data) => {
         feedback.html('');
         message.val('');
-        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+        console.log(data)
+        console.log(data.username)
+        chatroom.append("<div class='message'>" + data.username +  " ( "+ data.time + " )"+" ~ " + data.message + "</div>")
     })
 
     //Emit a username
-    send_username.click(function(){
-        socket.emit('change_username', {username : username.val()})
+    send_password.click(function(){
+        socket.emit('change_user', {username : username.val(), password : password.val()})
+        username.val("");
+        password.val("");
     })
 
     //Emit typing
@@ -35,5 +48,9 @@ $(function(){
     //Listen on typing
     socket.on('typing', (data) => {
         feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
+    })
+
+    socket.on('online_changed', (data) => {
+           $("#online_users").html("Online users: "+ data.online);
     })
 });
